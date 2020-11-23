054df314b19d086b41cd31ab7b40b206a966c03a
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-11-24 02:16:25 +0900

    [Add]Fragment
    개발 fragment add

---
+++ app/src/main/java/com/example/propofolteam/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.example.propofolteam
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        return view
+    }
+}
