9edf83ef4af64f3bd85435b7bd1906cc00f1abd1
YJH <chuchoun1234@gmail.com>
2020-11-24 16:47:59 +0900

    [UPDATE bug fix

--- app/src/main/java/com/example/propofolteam/view/LoginFragment.kt
+++ app/src/main/java/com/example/propofolteam/view/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.propofolteam.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.propofolteam.R
+import kotlinx.android.synthetic.main.fragment_intro.view.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment(){
@@ -18,6 +20,10 @@ class LoginFragment : Fragment(){
         val view = inflater.inflate(R.layout.fragment_login, container, false)
         view.login_have_account.setOnClickListener{
             findNavController().navigate(R.id.action_introFragment_to_signUpFragment)
+        }
+
+        view.login_btn.setOnClickListener {
+            startActivity(Intent(requireContext(), MainActivity::class.java))
         }
         return view
     }
