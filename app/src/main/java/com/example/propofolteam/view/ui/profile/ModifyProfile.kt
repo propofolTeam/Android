b6b9cc4b47fa65330468f683e4736c5c1828fd7a
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2020-11-24 17:17:39 +0900

    Update ModifyProfile.kt

--- app/src/main/java/com/example/propofolteam/view/ui/profile/ModifyProfile.kt
+++ app/src/main/java/com/example/propofolteam/view/ui/profile/ModifyProfile.kt
@@ -1,14 +1,19 @@
 package com.example.propofolteam.view.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.propofolteam.R
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
 
 class ModifyProfile : Fragment() {
-
+    private val GET_GALLERY_IMAGE = 200
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,6 +21,21 @@ class ModifyProfile : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.modify_profile, container, false)
 
+        val url =
+            "https://lh6.ggpht.com/9SZhHdv4URtBzRmXpnWxZcYhkgTQurFuuQ8OR7WZ3R7fyTmha77dYkVvcuqMu3DLvMQ=w300"
+        Glide.with(this)
+            .load(url)
+            .circleCrop()
+            .placeholder(R.mipmap.ic_launcher)
+            .into(view.profileImg)
+
+        view.modifyButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+            startActivity(intent)
+        }
+
         return view
     }
+
 }
