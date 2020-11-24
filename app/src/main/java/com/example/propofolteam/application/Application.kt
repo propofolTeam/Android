532f654f227feee21c888781679f1f3d405f9447
YJH <chuchoun1234@gmail.com>
2020-11-24 11:00:21 +0900

    [ADD] navigation

---
+++ app/src/main/java/com/example/propofolteam/application/Application.kt
@@ -0,0 +1,30 @@
+package com.example.propofolteam.application
+
+import android.app.Application
+import com.example.propofolteam.network.BaseUrl
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Application : Application(){
+
+    lateinit var retrofit: Retrofit
+    private var baseUrl : BaseUrl = BaseUrl()
+
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addNetworkInterceptor(StethoInterceptor())
+        val client = httpClient.build()
+
+         retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl.BaseURL)
+            .addConverterFactory(GsonConverterFactory.create())
+             .client(client)
+            .build()
+    }
+}
---
+++ app/src/main/java/com/example/propofolteam/network/BaseUrl.kt
@@ -0,0 +1,5 @@
+package com.example.propofolteam.network
+
+data class BaseUrl(
+    val BaseURL: String = "http://10.80.161.202:8080"
+)
