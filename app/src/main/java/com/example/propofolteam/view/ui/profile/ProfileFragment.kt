4855df4f04f93c102cd8cc70d0bf5adfc2167008
YJH <chuchoun1234@gmail.com>
2020-11-24 17:04:10 +0900

    [UPDATE] navigate

--- app/src/main/java/com/example/propofolteam/view/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/propofolteam/view/ui/profile/ProfileFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.propofolteam.R
-import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 
@@ -30,11 +27,8 @@ class ProfileFragment : Fragment() {
             .into(view.profileImg)
 
         view.modifyButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_modifyProfile)
         }
-
-        val myUpload = view.myUpload
-
-
 
         return view
     }
