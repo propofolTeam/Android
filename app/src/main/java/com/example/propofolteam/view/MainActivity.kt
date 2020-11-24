dfba61ba922fc24e5e1c1f78fc1835dac928d264
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2020-11-24 13:07:44 +0900

    internetpermission

--- app/src/main/java/com/example/propofolteam/view/MainActivity.kt
+++ app/src/main/java/com/example/propofolteam/view/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.propofolteam.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_profile)
     }
 }
